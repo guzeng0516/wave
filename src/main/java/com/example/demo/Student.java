d701a968013eb56afe5d511760c7838e03b320ff
guzeng <guzeng0516@gmail.com>
2020-09-17 18:14:24 -0400

    added in memory db and first entity

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String passportNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
